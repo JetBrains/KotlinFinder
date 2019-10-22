dda41c433ebd67980ee17533b5332099189da641
Anton Shelar <shelaranton@gmail.com>
2019-10-22 15:31:23 +0700

    JF-9 added dummy animation view

--- ios-app/src/iosMain/kotlin/Screens/SpotSearchViewController.kt
+++ ios-app/src/iosMain/kotlin/Screens/SpotSearchViewController.kt
@@ -1,7 +1,9 @@
 package Screens
 
+import Views.SpotDistanceView
 import common.centerInSuperview
 import common.fillSuperview
+import platform.CoreGraphics.CGRectMake
 import platform.Foundation.NSCoder
 import platform.UIKit.*
 
@@ -11,7 +13,7 @@ class SpotSearchViewController: UIViewController {
     private val statusLabel: UILabel = UILabel()
     private val instructionLabel: UILabel = UILabel()
     private val successImageView: UIImageView = UIImageView(UIImage.imageNamed("spotFound"))
-    private val spotSearchView: UIView = UIView()
+    private val spotSearchView: SpotDistanceView = SpotDistanceView(frame = CGRectMake(0.0, 0.0, 0.0, 0.0))
 
     @OverrideInit
     constructor() : super(nibName = null, bundle = null)
@@ -45,14 +47,12 @@ class SpotSearchViewController: UIViewController {
         this.statusLabel.font = UIFont.boldSystemFontOfSize(20.0)
         this.statusLabel.textAlignment = NSTextAlignmentCenter
         this.statusLabel.textColor = UIColor.colorNamed("blackTextColor")!!
-        this.statusLabel.text = "Searching..."
 
         this.instructionLabel.font = UIFont.systemFontOfSize(14.0)
         this.instructionLabel.textAlignment = NSTextAlignmentCenter
         this.instructionLabel.numberOfLines = 0
         this.instructionLabel.lineBreakMode = NSLineBreakByWordWrapping
         this.instructionLabel.textColor = UIColor.colorNamed("blackLightTextColor")!!
-        this.instructionLabel.text = "The more intense and stronger the vibration, the closer you are to the goal!"
 
         val labelsStackView: UIStackView = UIStackView()
         labelsStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -66,9 +66,21 @@ class SpotSearchViewController: UIViewController {
         labelsStackView.topAnchor.constraintEqualToAnchor(this.spotSearchStatusContainerView.bottomAnchor, constant = 30.0).setActive(true)
         labelsStackView.centerXAnchor.constraintEqualToAnchor(this.view.centerXAnchor).setActive(true)
         labelsStackView.widthAnchor.constraintLessThanOrEqualToAnchor(this.view.widthAnchor, multiplier = 0.8, constant = 0.0).setActive(true)
+
+        this.setSearchMode(true)
     }
 
     private fun setSearchMode(searchMode: Boolean) {
-
+        if (searchMode) {
+            this.statusLabel.text = "Searching..."
+            this.instructionLabel.text = "The more intense and stronger the vibration, the closer you are to the goal!"
+            this.successImageView.setHidden(true)
+            this.spotSearchView.setHidden(false)
+        } else {
+            this.statusLabel.text = "Spot found"
+            this.instructionLabel.text = "You are well done! Another letter in the control word is open"
+            this.successImageView.setHidden(false)
+            this.spotSearchView.setHidden(true)
+        }
     }
 }
