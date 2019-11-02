fdf0cf9ff0c78b77727e47e9353079616e3ef29f
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-11-02 13:51:57 +0700

    remove old files

--- ios-app/src/iosMain/kotlin/DeviceInformer.kt
+++
@@ -1,6 +0,0 @@
-data class DeviceRecord(val uuid: String, val name: String?, var rssi: Int, var lastSeen: Long)
-
-interface DeviceInformer {
-    fun bluetoothUnavailable()
-    fun informDevices(devices: List<DeviceRecord>)
-}
