e0d7fdd3070c1b67b2dad5161680d9ad0d8b7af6
Anton Shelar <shelar@siberian.pro>
2019-11-13 20:07:57 +0700

    JF-16 main screen layout

--- ios-app/watch Extension/MainInterfaceController.swift
+++ ios-app/watch Extension/MainInterfaceController.swift
@@ -11,13 +11,13 @@ import Foundation
 
 
 class MainInterfaceController: WKInterfaceController {
-    @IBOutlet var progressImage: WKInterfaceImage!
-    @IBOutlet var progressLabel: WKInterfaceLabel!
+    @IBOutlet private var progressImage: WKInterfaceImage!
+    @IBOutlet private var progressLabel: WKInterfaceLabel!
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+
+        self.setStepsCount(count: 0)
     }
 
     override func willActivate() {
@@ -33,6 +33,23 @@ class MainInterfaceController: WKInterfaceController {
     private func setStepsCount(count: Int) {
         self.progressImage.setImageNamed("kotlin\(count)")
 
+        self.progressLabel.setAttributedText(self.createCollectedString("KOTLIN", rangeEnd: count))
+    }
 
+    private func createCollectedString(_ str: String, rangeEnd: Int) -> NSAttributedString {
+        let attributedString: NSMutableAttributedString = NSMutableAttributedString(
+            string: str,
+            attributes: [.font: UIFont.boldSystemFont(ofSize: 30.0), .foregroundColor: UIColor.white.withAlphaComponent(0.2)])
+
+        attributedString.addAttributes([.foregroundColor: UIColor.white], range: NSMakeRange(0, rangeEnd))
+
+        return attributedString
+    }
+
+    private func presentCompletionAlert() {
+        self.presentAlert(withTitle: "All tasks were completed",
+                          message: "Please go to the mobile application to enter your name",
+                          preferredStyle: .alert,
+                          actions: [WKAlertAction(title: "Ok", style: .cancel, handler: {})])
     }
 }
