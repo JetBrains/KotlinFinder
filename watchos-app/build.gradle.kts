5537ac5316c78b38c8b6aeb1e4af418332635324
Anton Shelar <shelar@siberian.pro>
2019-11-14 16:07:02 +0700

    JF-17 mpp preparations

--- watchos-app/build.gradle.kts
+++ watchos-app/build.gradle.kts
@@ -1,7 +1,3 @@
-/*
- * Copyright 2019 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
- */
-
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
 
@@ -10,124 +6,52 @@ plugins {
     id("org.jetbrains.kotlin.multiplatform")
 }
 
-enum class Target(val simulator: Boolean, val key: String) {
-    WATCHOS_X86(true, "watchosX86"),
-    WATCHOS_ARM64(false, "watchosArm64")
-}
-
-val sdkName: String? = System.getenv("SDK_NAME") ?: "watchsimulator"
-
-val target = sdkName.orEmpty().let {
-    when {
-        it.startsWith("watchos") -> Target.WATCHOS_ARM64
-        else -> Target.WATCHOS_X86
-    }
-}
-
-val buildType = System.getenv("CONFIGURATION")?.let {
-    NativeBuildType.valueOf(it.toUpperCase())
-} ?: NativeBuildType.DEBUG
-
-
 kotlin {
-    val watchArm = watchosArm64()
-    val watchX = watchosX86()
+    targets {
+        val configureIos: KotlinNativeTarget.() -> Unit = {
+            binaries {
+                framework("MultiPlatformLibrary") {
 
-    // Declare the output program.
-    configure(listOf(watchArm, watchX)) {
-        binaries.executable(listOf(buildType)) {
-            baseName = "watchapp"
+                }
+            }
+        }
+
+        watchosArm64("watchosArm64", configureIos)
+        watchosX86("watchosX86", configureIos)
+    }
+
+    sourceSets {
+        val watchosArm64Main by getting {
+            kotlin.srcDir(file("src/watchosArm64Main/kotlin"))
+        }
+        val watchosX86Main by getting {
+            kotlin.srcDir(file("src/watchosX86Main/kotlin"))
         }
     }
 }
 
 dependencies {
-
+//    mppLibrary(Deps.Libs.MultiPlatform.kotlinStdLib)
+//    mppLibrary(Deps.Libs.MultiPlatform.coroutines)
 }
 
-val xcodeIntegrationGroup: String = "Xcode integration"
-val xcodeBundleId = "org.jetbrains.kotlin.native-demo"
+tasks.mapNotNull { it as? org.jetbrains.kotlin.gradle.tasks.KotlinNativeLink }
+    .mapNotNull { it.binary as? org.jetbrains.kotlin.gradle.plugin.mpp.Framework }
+    .forEach { framework ->
+        val linkTask = framework.linkTask
+        val syncTaskName = linkTask.name.replaceFirst("link", "sync")
+        val syncFramework = tasks.create(syncTaskName, Sync::class.java) {
+            group = "cocoapods"
 
-val targetBuildDir: String? = System.getenv("TARGET_BUILD_DIR")
-val executablePath: String? = System.getenv("EXECUTABLE_PATH")
+            val outputDir = file("build/cocoapods/framework")
 
-val currentTarget = kotlin.targets[target.key] as KotlinNativeTarget
-val kotlinBinary = currentTarget.binaries.getExecutable(buildType)
+            from(framework.outputDirectory)
+            into(outputDir)
 
-val packForXcode = if (sdkName == null || targetBuildDir == null || executablePath == null) {
-    // The build is launched not by Xcode ->
-    // We cannot create a copy task and just show a meaningful error message.
-    tasks.create("packForXCode").doLast {
-        throw IllegalStateException("Please run the task from Xcode")
+            doLast {
+                val file = file("${outputDir.path}/task")
+                file.writeText(syncTaskName)
+            }
+        }
+        syncFramework.dependsOn(linkTask)
     }
-} else {
-    // Otherwise copy the executable into the Xcode output directory.
-    tasks.create("packForXCode", Copy::class.java) {
-        dependsOn(kotlinBinary.linkTask)
-        destinationDir = file(targetBuildDir)
-        from(kotlinBinary.outputFile)
-        rename { executablePath }
-    }
-}
-
-val xcodeProject = file("ios-app.xcodeproj")
-val xcodeAppName = "ios-app"
-val xcodeDerivedDataPath = file("$buildDir/xcode-build")
-
-val startSimulator by tasks.creating(Exec::class.java) {
-    group = xcodeIntegrationGroup
-    description = "Starts an iOS simulator."
-
-    executable = "open"
-    args("/Applications/Xcode.app/Contents/Developer/Applications/Simulator.app")
-}
-
-val buildAppWithXcode by tasks.creating(Exec::class.java) {
-    dependsOn(kotlinBinary.linkTask)
-    group = xcodeIntegrationGroup
-    description = "Builds the iOS application bundle using Xcode."
-
-    workingDir = xcodeProject
-    executable = "sh"
-    args(
-        "-c", """
-        xcrun xcodebuild \
-            -scheme $xcodeAppName \
-            -project . \
-            -configuration Debug \
-            -destination 'platform=iOS Simulator,name=iPhone SE,OS=12.2' \
-            -derivedDataPath '$xcodeDerivedDataPath' \
-            CONFIGURATION_BUILD_DIR='$targetBuildDir'
-        """.trimIndent()
-    )
-}
-
-val installAppInSimulator by tasks.creating(Exec::class.java) {
-    dependsOn(buildAppWithXcode)
-
-    group = xcodeIntegrationGroup
-    description = "Installs the application bundle on an iOS simulator."
-    executable = "sh"
-
-    val appFolder = "${targetBuildDir}/${xcodeAppName}.app"
-
-    args("-c", "xcrun simctl install booted '${appFolder}'")
-}
-
-val launchAppInSimulator by tasks.creating(Exec::class.java) {
-    dependsOn(installAppInSimulator)
-
-    group = xcodeIntegrationGroup
-    description = "Launches the application on an iOS simulator."
-
-    executable = "sh"
-    args("-c", "xcrun simctl launch booted $xcodeBundleId")
-}
-
-tasks.create("run") {
-    group = xcodeIntegrationGroup
-
-    dependsOn(buildAppWithXcode)
-    dependsOn(installAppInSimulator)
-    dependsOn(launchAppInSimulator)
-}
