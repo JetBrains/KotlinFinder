7190d6b0e99e30eda41ce901b2ae9c4db915ea00
Aleksey Mikhailov <Aleksey.Mikhailov@icerockdev.com>
2019-12-01 14:26:20 +0700

    up android minsdk (necessary for anims)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     object Android {
         const val compileSdk = 28
         const val targetSdk = 28
-        const val minSdk = 16
+        const val minSdk = 21
     }
 
     const val kotlin = "1.3.60"
